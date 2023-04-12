227728c66a3b3580f03179660f75709f78d7d152
Navin Avadhani <knavinavadhani@gmail.com>
2023-04-12 18:31:45 +0530

    First commit. Hello world!

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="text-4xl font-bold text-secondary bg-primary w-fit m-6 p-5 rounded-lg mx-auto">
+      Hello World!
+    </div>
+  );
+}
+
+export default Home;
